395ed3c67b6006017a36103aca162062bb652c4f
aseminario <e3corpion@gmail.com>
2021-01-07 12:21:16 -0500

    se agrego migraciones a tipo de cambio

---
+++ database/migrations/2021_01_07_121418_create_exchanges_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExchangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchanges', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->decimal('compra',4,3);
+            $table->decimal('venta',4,3);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchanges');
+    }
+}
