d987dde41de37fc1066785c4cbbf70c00159e9f5
aseminario <e3corpion@gmail.com>
2021-01-07 13:25:50 -0500

    se modifico kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -20,22 +21,26 @@ class Kernel extends ConsoleKernel
     protected function scheduleDailyCommands(Schedule $schedule) {
        // $schedule->command('products:set-rating')->dailyAt('01:30');
         $schedule->command('down --message="Estamos en Mantenimiento (30 min)" --retry=30')->dailyAt(env('MAINTENANCE_HOUR'));
-        
-        $schedule->command('tax_payers:get_data')->dailyAt(env('MAINTENANCE_HOUR')); 
-        $schedule->command('tax_payers:truncate')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () {  
+
+        $schedule->command('Exchange:today')->dailyAt(env('MAINTENANCE_HOUR'));
+
+        $schedule->command('tax_payers:get_data')->dailyAt(env('MAINTENANCE_HOUR'));
+        $schedule->command('tax_payers:truncate')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () {
             return Storage::disk('padron_reducido')->exists('padron_reducido_ruc.zip'); //.text
         });
-        $schedule->command('tax_payers:load_data')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () { 
-           return Storage::disk('padron_reducido')->exists('padron_reducido_ruc.zip'); 
-        }); 
+        $schedule->command('tax_payers:load_data')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () {
+           return Storage::disk('padron_reducido')->exists('padron_reducido_ruc.zip');
+        });
 
-        $schedule->command('local_anex:get_data')->dailyAt(env('MAINTENANCE_HOUR')); 
-        $schedule->command('local_anex:truncate')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () {  
+        $schedule->command('local_anex:get_data')->dailyAt(env('MAINTENANCE_HOUR'));
+        $schedule->command('local_anex:truncate')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () {
             return Storage::disk('local_anexo')->exists('padron_reducido_local_anexo.txt'); //.text
         });
-        $schedule->command('local_anex:load_data')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () { 
-        return Storage::disk('local_anexo')->exists('padron_reducido_local_anexo.txt'); 
+        $schedule->command('local_anex:load_data')->dailyAt(env('MAINTENANCE_HOUR'))->when(function () {
+        return Storage::disk('local_anexo')->exists('padron_reducido_local_anexo.txt');
         });
+
+
 
         $schedule->command('up')->dailyAt(env('MAINTENANCE_HOUR'));
 
